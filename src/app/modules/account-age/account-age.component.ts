eea784264e7ad7591740a45b5b138a8f885491ff
Lou Regla <lou.regla@gofullscale.com>
2018-12-01 02:54:58 -0800

    AccountAgeModule: implement api fetch and hours calculation

--- src/app/modules/account-age/account-age.component.ts
+++ src/app/modules/account-age/account-age.component.ts
@@ -14,13 +14,36 @@ import { LoginReferrerService } from '../../services/login-referrer.service';
 
 export class AccountAgeComponent {
 
-  // minds = window.Minds;
+  minds = window.Minds;
+  hours: number;
 
   constructor(
     public client: Client,
     public title: MindsTitle,
   ) {
     this.title.setTitle('How Many Hours?');
+  }
+
+  ngOnInit() {
+    this.loadTimeData();
+  }
+
+  private loadTimeData() {
+    this.client.get('api/v2/howmanyhours')
+      .then((result: {status: string, seconds: number}) => {
+        let timestamp;
+
+        if (result.status === 'success') {
+            timestamp = result.seconds;
+            this.hours = this.getHours(timestamp);
+        }
+      })
+      .catch((error) => console.error(error));
+  }
+
+  private getHours(timestamp: number) {
+    const difference = Date.now() - (timestamp * 1000);
+    return Math.floor(difference / 3600000);
   }
 
 }
